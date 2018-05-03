ac39012b61d076e16f9647ec17c0492b5c2a370a
ComputerJunkie <7307177+ComputerJunkie@users.noreply.github.com>
2018-05-02 21:34:29 -0500

    Add files via upload

---
+++ mainPart2.js
@@ -0,0 +1,12 @@
+$(function() {
+  let open = $("#open");
+  let close = $("#close");
+  let side = $(".nav");
+
+  open.click(function() {
+    side.toggleClass("open");
+  });
+  $("main").click(function() {
+    side.removeClass("open");
+  });
+});
