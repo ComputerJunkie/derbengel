56d1354f473e32e4ee16a70b89998f11f5e6f7c8
ComputerJunkie <7307177+ComputerJunkie@users.noreply.github.com>
2018-05-08 17:35:39 -0500

    Add files via upload

---
+++ MyPixArtMaker/designs.js
@@ -0,0 +1,38 @@
+// Select color input
+// Select size input
+var height, width, color;
+
+// When size is submitted by the user, call makeGrid()
+$("#sizePicker").submit(function(event) {
+    event.preventDefault();
+    height = $("#inputHeight").val();
+    width = $("#inputWidth").val();
+    makeGrid(height, width);
+   //console.log("height: " +  height + " and width: " + width);
+})
+
+
+function makeGrid(x, y) {
+    $("tr").remove();
+	
+// Your code goes here!
+    for (var i = 1; i<= x; i++) {
+        $("#pixelCanvas").append("<tr id=table" + i + "></tr>");
+        for (var c =1; c<= y; c++) {
+            $("#table" + i).append("<td></td>");
+        }
+		
+    }
+	
+    // add color to cell
+    $("td").click(function addColor() {
+        color = $("#colorPicker").val();
+		
+        if ($(this).attr("style")) {
+            $(this).removeAttr("style")
+        } else {
+            $(this).attr("style", "background-color:" + color);
+        }
+    });
+
+}
